0fb419925ceda06dd543c2a414e533c4395d6fd6
Nassim <nassim.et@hotmail.fr>
2018-05-29 00:01:45 +0200

    Create Knn Object

--- spamdetector/Main.py
+++ spamdetector/Main.py
@@ -1,8 +1,12 @@
-
 from spamdetector.cleanData.CleanData import CleanData
 from spamdetector.algorithme.NaiveBayes import NaiveBayes
+from spamdetector.algorithme.KNene import Knn
 
 if __name__ == '__main__':
     clean_data = CleanData()
+
     naiveBayes = NaiveBayes(clean_data.getDf(), clean_data.getNames())
     naiveBayes.detailed_result()
+
+    knn = Knn(clean_data.getDf(), clean_data.getNames(), 0.20, 4)
+    knn.standardisation()
---
+++ spamdetector/algorithme/KNene.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from spamdetector.cleanData.Statistics import Statistics
+import numpy as np
+
+
+class Knn:
+
+    def __init__(self, df, names, test_size, neighbors):
+        self.emailsClean = pd.DataFrame(df, columns=names)
+        self.test_size = test_size
+        self.neighbors = neighbors
+        self.statistics = Statistics()
+
+    def getX(self):
+        np.set_printoptions(suppress=True, threshold=np.nan)
+        labels = []
+        df = (self.statistics.getStatisticsWithWords())\
+            .query('var>700 | (average>0.1 & var<=300) | index=="is_spam"')
+        for i in df.index:
+            labels.append(i)
+        X = self.emailsClean
+        X = X.drop(labels=labels, axis=1)
+        return X
+
+    def getY(self):
+        y = self.emailsClean["is_spam"]
+        y = y.transpose()
+        return y
+
+    def standardisation(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.getX(), self.getY(), test_size=self.test_size)
+
+        scaler = StandardScaler()
+        scaler.fit(X_train)
+
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        classifier = KNeighborsClassifier(n_neighbors=self.neighbors)
+        classifier.fit(X_train, y_train)
+
+        y_pred = classifier.predict(X_test)
+
+        print()
+        print("************************* Knn Kawaii Results ~ <3 *****************************")
+        print(classification_report(y_test, y_pred))
