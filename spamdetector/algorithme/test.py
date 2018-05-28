e267b6d736997163d28593e06aa184b38b46c2df
KatiaAmichi <amichi.katia@gmail.com>
2018-05-28 17:15:43 +0200

    [algo] First version of Naive Bayes algo working, 97% success

--- spamdetector/algorithme/test.py
+++ spamdetector/algorithme/test.py
@@ -4,3 +4,16 @@ import pprint
 import pandas as pd
 
 
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+X = np.array([[-1, -1], [-2, -1]])
+Y = np.array([0,1])
+clf = GaussianNB()
+clf.fit(X, Y)
+print("1 : ", clf.predict([[-0.8, -1]]))
+#clf_pf = GaussianNB()
+#clf_pf.partial_fit(X, Y, np.unique(Y))
+#print("1 : ", clf_pf.predict([[-0.8, -1]]))
+
