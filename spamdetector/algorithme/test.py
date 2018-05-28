ed2635123689fae8a5351e2a086c44b0a81ed1e4
KatiaAmichi <amichi.katia@gmail.com>
2018-05-28 13:32:17 +0200

    change of clean-data

--- spamdetector/algorithme/test.py
+++ spamdetector/algorithme/test.py
@@ -1,18 +1,6 @@
 # -*- coding: utf-8 -*-
-
-from pathlib import Path
-#from sklearn import datasets
+import csv
+import pprint
 import pandas as pd
 
-#iris = datasets.load_iris()
-data = str(Path.home()) + '\IdeaProjects\dataScience\dataset\spambase.data'
 
-#file = open(data, 'r', delimiter=',')
-
-dataPanda = pd.read_csv(data)
-
-nb = 0
-
-print(dataPanda.iloc[:, -1])
-
-print("nb : ",  nb)
--- spamdetector/cleanData/clean-data.py
+++ spamdetector/cleanData/clean-data.py
@@ -1,68 +1,23 @@
+# -*- coding: utf-8 -*-
+import csv
+import pprint
 import pandas as pd
 
-tag = [
-'word_freq_make',
-'word_freq_address',
-'word_freq_all',
-'word_freq_3d',
-'word_freq_our',
-'word_freq_over',
-'word_freq_remove',
-'word_freq_internet',
-'word_freq_order',
-'word_freq_mail',
-'word_freq_receive',
-'word_freq_will',
-'word_freq_people',
-'word_freq_report',
-'word_freq_addresses',
-'word_freq_free',
-'word_freq_business',
-'word_freq_email',
-'word_freq_you',
-'word_freq_credit',
-'word_freq_your',
-'word_freq_font',
-'word_freq_000',
-'word_freq_money',
-'word_freq_hp',
-'word_freq_hpl',
-'word_freq_george',
-'word_freq_650',
-'word_freq_lab',
-'word_freq_labs',
-'word_freq_telnet',
-'word_freq_857',
-'word_freq_data',
-'word_freq_415',
-'word_freq_85',
-'word_freq_technology',
-'word_freq_1999',
-'word_freq_parts',
-'word_freq_pm',
-'word_freq_direct',
-'word_freq_cs',
-'word_freq_meeting',
-'word_freq_original',
-'word_freq_project',
-'word_freq_re',
-'word_freq_edu',
-'word_freq_table',
-'word_freq_conference',
-'char_freq_;',
-'char_freq_(',
-'char_freq_[',
-'char_freq_!',
-'char_freq_$',
-'char_freq_#',
-'capital_run_length_average',
-'capital_run_length_longest',
-'capital_run_length_total',
-'is_spam'
-]
+FILE_spambase_header_names = '../rawData/spambase.header_names'
+FILE_spambase_data = '..//rawData//spambase.data'
 
-tests = pd.read_csv("spambase.data", names=tag)
-print(tests)
+names = []
 
-#df = pd.DataFrame(tests, columns = tag)
-#print(df)
+with open(FILE_spambase_header_names, 'rt') as f:
+    names = [name.strip() for name in f.readlines()]
+
+spambase_data = pd.read_csv(FILE_spambase_data, names=names)
+df = pd.DataFrame(spambase_data, columns=names)
+
+def data_spam():
+    return df[df['is_spam'] == 1].describe().unstack()
+
+def data_no_spam():
+    return df[df['is_spam'] == 0].describe().unstack()
+
+pprint.pprint(data_spam().max)
