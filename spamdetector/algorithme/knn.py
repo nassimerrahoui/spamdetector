f1531f7b7c64d16796a2d57e1e08585c80840bc1
Nassim <nassim.et@hotmail.fr>
2018-05-28 16:40:30 +0200

    ajout du gitingore

---
+++ spamdetector/algorithme/knn.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+#tag
+tag = [
+    'word_freq_make',
+    'word_freq_address',
+    'word_freq_all',
+    'word_freq_3d',
+    'word_freq_our',
+    'word_freq_over',
+    'word_freq_remove',
+    'word_freq_internet',
+    'word_freq_order',
+    'word_freq_mail',
+    'word_freq_receive',
+    'word_freq_will',
+    'word_freq_people',
+    'word_freq_report',
+    'word_freq_addresses',
+    'word_freq_free',
+    'word_freq_business',
+    'word_freq_email',
+    'word_freq_you',
+    'word_freq_credit',
+    'word_freq_your',
+    'word_freq_font',
+    'word_freq_000',
+    'word_freq_money',
+    'word_freq_hp',
+    'word_freq_hpl',
+    'word_freq_george',
+    'word_freq_650',
+    'word_freq_lab',
+    'word_freq_labs',
+    'word_freq_telnet',
+    'word_freq_857',
+    'word_freq_data',
+    'word_freq_415',
+    'word_freq_85',
+    'word_freq_technology',
+    'word_freq_1999',
+    'word_freq_parts',
+    'word_freq_pm',
+    'word_freq_direct',
+    'word_freq_cs',
+    'word_freq_meeting',
+    'word_freq_original',
+    'word_freq_project',
+    'word_freq_re',
+    'word_freq_edu',
+    'word_freq_table',
+    'word_freq_conference',
+    'char_freq_;',
+    'char_freq_(',
+    'char_freq_[',
+    'char_freq_!',
+    'char_freq_$',
+    'char_freq_#',
+    'capital_run_length_average',
+    'capital_run_length_longest',
+    'capital_run_length_total',
+    'is_spam'
+]
+
+# import some data to play with
+emailsRaw = str(Path.home()) + "\IdeaProjects\spamdetector\spamdetector\dataRaw\spambase.data"
+emails = pd.read_csv(emailsRaw, names=tag)
+emailsClean = pd.DataFrame(emails, columns=tag)
+np.set_printoptions(suppress=True, threshold=np.nan)
+
+X = emailsClean
+X = X.drop(labels=["is_spam", "word_freq_make", "word_freq_george", "word_freq_1999", "char_freq_(", ""], axis=1)
+y = emailsClean["is_spam"]
+y = y.transpose()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors=4)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print(y_pred)
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+error = []
+
+#Calculating error for K values between 1 and 40
+
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    pred_i = knn.predict(X_test)
+    error.append(np.mean(pred_i != y_test))
+
+print(error)
+
+"""
+plt.figure(figsize=(12, 6))
+plt.plot(range(1, 3071), error, color='red', linestyle='dashed', marker='o',
+         markerfacecolor='blue', markersize=10)
+plt.title('Error Rate K Value')
+plt.xlabel('K Value')
+plt.ylabel('Mean Error')
+"""
