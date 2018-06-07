efd2e7efac1d83af2b80c4ea17ba722ce8941435
Nassim <nassim.et@hotmail.fr>
2018-06-07 17:47:18 +0200

    Kernel SVM

--- spamdetector/Main.py
+++ spamdetector/Main.py
@@ -1,6 +1,7 @@
 from spamdetector.cleanData.CleanData import CleanData
 from spamdetector.algorithme.NaiveBayes import NaiveBayes
 from spamdetector.algorithme.KNene import Knn
+from spamdetector.algorithme.KernelSVM import KernelSVM
 
 if __name__ == '__main__':
     clean_data = CleanData()
@@ -8,5 +9,12 @@ if __name__ == '__main__':
     naiveBayes = NaiveBayes(clean_data.getDf(), clean_data.getNames())
     naiveBayes.detailed_result()
 
+    # Neighboors define classes of a row
     knn = Knn(clean_data.getDf(), clean_data.getNames(), 0.20, 5)
     knn.standardisation()
+
+    # Kernel SVM seperate classes by a frontier in a dimension with Kernel method
+    # linear trace straight frontier
+    # rbf trace straight frontier but accept errors
+    kernelSVM = KernelSVM(clean_data.getDf(), clean_data.getNames(), 0.20)
+    kernelSVM.results("linear")  # you can set at "linear" or "rbf"
