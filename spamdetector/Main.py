2644c5e274af1563d3f19b2b08993ccfa78069b3
Nassim <nassim.et@hotmail.fr>
2018-06-09 12:32:25 +0200

    Comments some lines

--- spamdetector/Main.py
+++ spamdetector/Main.py
@@ -9,7 +9,7 @@ from spamdetector.algorithme.RandomForest import RandomForest
 from spamdetector.cleanData.CleanData import CleanData
 from spamdetector.cleanData.Training import Training
 from spamdetector.unsupervised.Classification_hiérarchique import Classification_hiérarchique
-from spamdetector.unsupervised.Tsne import Tsne
+# from spamdetector.unsupervised.Tsne import Tsne
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import pandas as pd
@@ -27,10 +27,10 @@ if __name__ == '__main__':
     training = Training(FILE_spambase_header_names, FILE_spambase_data)
 
 
-    """Backpropagation"""
-    print("############Backpropagation############")
-    backpropagation = Backpropagation(training,FILE_spambase_data)
-    backpropagation_acc = backpropagation.data_file()
+    # """Backpropagation"""
+    # print("############Backpropagation############")
+    # backpropagation = Backpropagation(training,FILE_spambase_data)
+    # backpropagation_acc = backpropagation.data_file()
 
     """LogisticRegression"""
     print("############LogisticRegression############")
@@ -70,61 +70,61 @@ if __name__ == '__main__':
     print("-------------------------------------------")
     print("")
 
-    """Tsne"""
-    print("############Tsne############")
-    #tsne = Tsne(load_data, FILE_img)
-    #tsne.result()
-    print("-------------------------------------------")
-    print("")
+    # """Tsne"""
+    # print("############Tsne############")
+    # #tsne = Tsne(load_data, FILE_img)
+    # #tsne.result()
+    # print("-------------------------------------------")
+    # print("")
+    #
+    # print("Histogramme")
+    # accuracy = [backpropagation_acc, logregression_acc, randomforest_acc, naivebayes_acc, knn_acc, svm_acc]
+    # accuracy_list = pd.Series.from_array(accuracy)
+    # accuracy_labels = ["Backpropagation", "LogisticRegression", "RandomForest", "NaiveBayes", "Knn", "Kernel SVM"]
+    #
+    # plt.figure(figsize=(12, 8))
+    # ax = accuracy_list.plot(kind='bar')
+    # ax.set_title('Accuracy des algos utilisés')
+    # ax.set_xlabel('Noms des algos')
+    # ax.set_ylabel('Accuracy')
+    # ax.set_xticklabels(accuracy_labels)
+    #
+    # ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2f'))
 
-    print("Histogramme")
-    accuracy = [backpropagation_acc, logregression_acc, randomforest_acc, naivebayes_acc, knn_acc, svm_acc]
-    accuracy_list = pd.Series.from_array(accuracy)
-    accuracy_labels = ["Backpropagation", "LogisticRegression", "RandomForest", "NaiveBayes", "Knn", "Kernel SVM"]
-
-    plt.figure(figsize=(12, 8))
-    ax = accuracy_list.plot(kind='bar')
-    ax.set_title('Accuracy des algos utilisés')
-    ax.set_xlabel('Noms des algos')
-    ax.set_ylabel('Accuracy')
-    ax.set_xticklabels(accuracy_labels)
-
-    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2f'))
-
-    rects = ax.patches
+    # rects = ax.patches
 
 
-    for rect in rects:
-         # Get X and Y placement of label from rect.
-        y_value = rect.get_height()
-        x_value = rect.get_x() + rect.get_width() / 2
-
-        # Number of points between bar and label. Change to your liking.
-        space = 5
-        # Vertical alignment for positive values
-        va = 'bottom'
-
-        # If value of bar is negative: Place label below bar
-        if y_value < 0:
-            # Invert space to place label below
-            space *= -1
-            # Vertically align label at top
-            va = 'top'
-
-        # Use Y value as label and format number with one decimal place
-        label = "{:.2f}".format(y_value)
-
-        # Create annotation
-        plt.annotate(
-            label,                      # Use `label` as label
-            (x_value, y_value),         # Place label at end of the bar
-            xytext=(0, space),          # Vertically shift label by `space`
-            textcoords="offset points", # Interpret `xytext` as offset in points
-            ha='center',                # Horizontally center label
-            va=va)                      # Vertically align label differently for
-                                        # positive and negative values.
-
-    plt.show()
+    # for rect in rects:
+    #      # Get X and Y placement of label from rect.
+    #     y_value = rect.get_height()
+    #     x_value = rect.get_x() + rect.get_width() / 2
+    #
+    #     # Number of points between bar and label. Change to your liking.
+    #     space = 5
+    #     # Vertical alignment for positive values
+    #     va = 'bottom'
+    #
+    #     # If value of bar is negative: Place label below bar
+    #     if y_value < 0:
+    #         # Invert space to place label below
+    #         space *= -1
+    #         # Vertically align label at top
+    #         va = 'top'
+    #
+    #     # Use Y value as label and format number with one decimal place
+    #     label = "{:.2f}".format(y_value)
+    #
+    #     # Create annotation
+    #     plt.annotate(
+    #         label,                      # Use `label` as label
+    #         (x_value, y_value),         # Place label at end of the bar
+    #         xytext=(0, space),          # Vertically shift label by `space`
+    #         textcoords="offset points", # Interpret `xytext` as offset in points
+    #         ha='center',                # Horizontally center label
+    #         va=va)                      # Vertically align label differently for
+    #                                     # positive and negative values.
+    #
+    # # plt.show()
     """Tsne"""
     print("############iris_df############")
     # Neighboors define classes of a row
